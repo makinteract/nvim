1290b1063ca306c80f5f996713fc6890e6e6de24
Andrea <andrea@kaist.ac.kr>
2024-02-12 18:47:37 +1100

    WIP

--- lua/plugins/treesitter/treesitter.lua
+++ lua/plugins/treesitter/treesitter.lua
@@ -5,11 +5,13 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = { "c", 
+        ensure_installed = { 
+          "c", 
           "lua", 
           "vim", "vimdoc", 
           "query", 
-          "javascript", "typescript", 
+          -- "javascript", 
+          -- "typescript", 
           "markdown",
           "css", "html" 
         },
